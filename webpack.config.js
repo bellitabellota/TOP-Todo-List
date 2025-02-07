b7673b8036fe389d00c906fa9199a184c28bcaaf
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-02-07 15:06:39 +0000

    Install and configure HTMLWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -8,4 +9,9 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
 };
