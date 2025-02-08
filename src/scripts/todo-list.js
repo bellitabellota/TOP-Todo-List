f148fc0c1f4f88c5daac3e01157f47e6d7613af2
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-02-08 16:09:46 +0000

    Expand TodoList class with addTodo() method

--- src/scripts/todo-list.js
+++ src/scripts/todo-list.js
@@ -1,3 +1,5 @@
+import { Todo } from "./todo";
+
 export class TodoList {
   constructor (name, description = null) {
     this.name = name;
@@ -7,5 +9,10 @@ export class TodoList {
 
   static create(name, description) {
     return new TodoList(name, description);
+  }
+
+  addTodo(name, details, dueDate, priority) {
+    const todo = TodoList.create(name, details, dueDate, priority);
+    this.todos.push[todo];
   }
 }
