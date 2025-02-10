64b71b2a331d56aefbce8d418100b7b08b215215
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-02-10 13:38:32 +0000

    Move displayLists to screenController

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import "./styles.css";
 import "modern-normalize";
 import { ListCollection } from "./scripts/list-collection";
-import { displayLists } from "./scripts/display-lists";
 import Screen from "./scripts/screen-controller";
 
 const listCollection = new ListCollection();
+const listsContainer = document.querySelector(".lists-container");
 
 Screen.generateDefaultTodoList(listCollection);
-
-const listsContainer = document.querySelector(".lists-container");
-displayLists(listCollection.all, listsContainer);
+Screen.displayLists(listCollection.all, listsContainer);
 
 const newListButton = document.querySelector(".js-new-list-button");
 const newListInput = document.querySelector(".js-new-list-input");
@@ -23,5 +21,5 @@ newListButton.addEventListener("click", (event) => {
 
   listCollection.addList(newListInput.value);
   newListInput.value = "";
-  displayLists(listCollection.all, listsContainer);
+  Screen.displayLists(listCollection.all, listsContainer);
 })
--- src/scripts/screen-controller.js
+++ src/scripts/screen-controller.js
@@ -1,3 +1,6 @@
+import deleteSvg from "../img/trash-can-outline.svg";
+import pencilSvg from "../img/pencil-outline.svg";
+
 const screenController = (function() {
   const generateDefaultTodoList = (listCollection) => {
     listCollection.addList("General");
@@ -9,7 +12,39 @@ const screenController = (function() {
     listCollection.all[0].addTodo("High Priority Todos", "Todos with a high priority are displayed in red. The color red unambiguously indicates the urgency of that Todo item.", "2024-04-20", "high");
   }
 
-  return { generateDefaultTodoList };
+  const displayLists = (lists, listsContainer) => {
+    let listHTML = "";
+    
+    lists.forEach(list => {
+      let listBodyHtml = "";
+      list.todos.forEach( todo => {
+        listBodyHtml += 
+          `<div class="todo ${todo.priority}">
+            <p class="todo-name">${todo.name}</p>
+            <p class="todo-due-date">${todo.dueDate}</p>
+            <p class="todo-details">${todo.details}</p>
+            <button class="edit-button"><img src="${pencilSvg}"></button>
+          </div>
+        `;
+    });
+  
+    listHTML += 
+      `<div class="list">
+        <div class="list-heading-container">
+          <h2>${list.name}</h2>
+          <button class="delete-button"><img src="${deleteSvg}" alt="delete-icon"></button>
+        </div>
+        <hr>
+        <div class="list-body">${listBodyHtml}</div>
+        <button class="new-todo-button">Add Todo</button>
+      </div>
+      `;
+    })
+  
+    listsContainer.innerHTML = listHTML;
+  }
+
+  return { generateDefaultTodoList, displayLists };
 })();
 
 export default screenController;
