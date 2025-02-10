ddc3f24203cbb48dd3c9bdfb6dbcd2c99955c208
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-02-10 13:06:04 +0000

    Add EventListener to add new list

--- src/index.js
+++ src/index.js
@@ -5,14 +5,13 @@ import { displayLists } from "./scripts/display-lists";
 
 const listCollection = new ListCollection();
 listCollection.addList("General");
-console.log(listCollection.all[0]);
 
 listCollection.all[0].addTodo("Spring cleaning", "This year don't forget to put away your boots. Otherwise you will again not be able to hesitate to wear them in the Summer.", "2024-04-20", "low");
 
 listCollection.all[0].addTodo("Spring cleaning", "This year don't forget to put away your boots. Otherwise you will again not be able to hesitate to wear them in the Summer.", "2024-04-20", "medium");
 
 listCollection.all[0].addTodo("Spring cleaning", "This year don't forget to put away your boots. Otherwise you will again not be able to hesitate to wear them in the Summer.", "2024-04-20", "high");
-console.log(listCollection.all[0].todos[0]);
+
 
 const listsContainer = document.querySelector(".lists-container");
 
@@ -21,3 +20,18 @@ listCollection.addList("Sample");
 listCollection.all[1].addTodo("Planning Summer Vacations", "Book Flight, take time of. Ask neighbors to water plants and feed cats.", "2024-04-20", "high");
 
 displayLists(listCollection.all, listsContainer);
+
+const newListButton = document.querySelector(".js-new-list-button");
+const newListInput = document.querySelector(".js-new-list-input");
+
+newListButton.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (newListInput.value === "") {
+    return alert("Input cannot be empty.");
+  }
+
+  listCollection.addList(newListInput.value);
+  newListInput.value = "";
+  displayLists(listCollection.all, listsContainer);
+})
