f74b2064f8c5c78fdc7215e18faddb6f085d7e6d
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-02-12 15:48:01 +0000

    Make date parameters in generateDefault Date objects

--- src/scripts/screen-controller.js
+++ src/scripts/screen-controller.js
@@ -14,11 +14,11 @@ class ScreenController {
   generateDefaultTodoList() {
     this.listCollection.addList("General");
   
-    this.listCollection.all[0].addTodo("Low Priority Todos", "Low Priority Todos are displayed with a yellow background color. The color yellow stands for happiness, optimism and hope.", "2024-04-20", "low");
+    this.listCollection.all[0].addTodo("Low Priority Todos", "Low Priority Todos are displayed with a yellow background color. The color yellow stands for happiness, optimism and hope.", new Date("2024-04-20"), "low");
   
-    this.listCollection.all[0].addTodo("Medium Priority Todos", "Todos with a medium priority are displayed in orange. Orange signals warmth and comfort.", "2024-04-20", "medium");
+    this.listCollection.all[0].addTodo("Medium Priority Todos", "Todos with a medium priority are displayed in orange. Orange signals warmth and comfort.", new Date("2024-04-20"), "medium");
   
-    this.listCollection.all[0].addTodo("High Priority Todos", "Todos with a high priority are displayed in red. The color red unambiguously indicates the urgency of that Todo item.", "2024-04-20", "high");
+    this.listCollection.all[0].addTodo("High Priority Todos", "Todos with a high priority are displayed in red. The color red unambiguously indicates the urgency of that Todo item.", new Date("2024-04-20"), "high");
   }
 
   renderLists() {
